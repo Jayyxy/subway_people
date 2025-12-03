259af38d10ede2c7d6170f8083b776f3a007d361
test <test@test.com>
2025-12-03 17:15:18 +0900

    data collect

---
+++ dataset.py
@@ -0,0 +1,7 @@
+# 레시피 DB 가져오기 
+
+# pip install requests pandas python-dotenv 설치 
+import requests
+
+url= "http://openapi.foodsafetykorea.go.kr/api/keyId/serviceId/dataType/startIdx/endIdx"
+key="96427e74c8f74077ab07"
